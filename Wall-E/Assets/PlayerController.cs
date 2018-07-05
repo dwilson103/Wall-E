31b1bc18ee386519a9db888526317b8a9fc4908e
Dan Wilson <danwilson369@hotmail.co.uk>
2018-07-05 20:15:23 +0100

    add movement and others

---
+++ Wall-E/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private float X_input;
+	private float Y_input;
+
+	public float player_speed;
+
+	private Rigidbody2D player_rigid_body;
+
+	private Animator player_animator;
+
+	// Use this for initialization
+	void Start () {
+		player_rigid_body = GetComponent<Rigidbody2D>();
+		player_animator = GetComponent<Animator>();
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+		X_input = Input.GetAxisRaw("Horizontal");
+		Y_input = Input.GetAxisRaw("Vertical");
+
+				
+
+		if((X_input != 0) || (Y_input != 0)) {
+			player_animator.SetFloat("X_input",X_input);
+			player_animator.SetFloat("Y_input",Y_input);
+			player_animator.Play("MoveAnimation");
+		}
+		else {
+			player_animator.Play("IdleAnimation");
+		}
+
+
+	
+		player_rigid_body.velocity = new Vector2(X_input, Y_input)*player_speed;
+	}
+
+	
+}
