8089f91d27af143e1176e61e8c22bb4f1ce67b99
Dan Wilson <danwilson369@hotmail.co.uk>
2018-07-08 20:46:18 +0100

    simplified layering script

--- Wall-E/Assets/Scripts/LayerController.cs
+++ Wall-E/Assets/Scripts/LayerController.cs
@@ -3,37 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LayerController : MonoBehaviour {
-	public GameObject player;
-
-	private const string SORTING_LAYER_BACKGROUND = "Environment";
-	private const string SORTING_LAYER_FOREGROUND = "Foreground";
-	private const int SORTING_ORDER = 1;
-
-	private SpriteRenderer objectSprite;
-
-	private Vector3 playerPosition;
-	private Vector3 objectPosition;
 
 	void Start() {
-		objectSprite = GetComponent<SpriteRenderer>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		playerPosition = player.transform.position;
-		objectPosition = transform.position;
-		Debug.Log(playerPosition.y);
-		Debug.Log(objectPosition.y);
-
-		DynamicLayerer();
+			GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y*100)*-1;
 	}
 
-	void DynamicLayerer() {
-		if(playerPosition.y > objectPosition.y) {
-			objectSprite.sortingLayerName = SORTING_LAYER_FOREGROUND;
-		} else {
-			objectSprite.sortingLayerName = SORTING_LAYER_BACKGROUND;
-		}
-			objectSprite.sortingOrder = SORTING_ORDER;
-	}
 }
