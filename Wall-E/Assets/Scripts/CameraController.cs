a9d6b65621e107f174f57c65eb1a50752c2f4cb2
Dan Wilson <danwilson369@hotmail.co.uk>
2018-07-19 20:56:25 +0100

    adds new area, spawn point and player/camera persist

--- Wall-E/Assets/Scripts/CameraController.cs
+++ Wall-E/Assets/Scripts/CameraController.cs
@@ -8,10 +8,20 @@ public class CameraController : MonoBehaviour {
 
 	private Vector3 offset;
 
+	private static bool cameraExists;
+
 	// Use this for initialization
 	void Start () {
 		
 		offset = transform.position - player.transform.position;
+
+		if(!cameraExists) {
+			cameraExists = true;
+			DontDestroyOnLoad(transform.gameObject);
+		}
+		else {
+			Destroy(gameObject);
+		}
 	}
 	
 	// Update is called once per frame
