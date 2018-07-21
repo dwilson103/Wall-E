35e74ec906083a1a4139204710672e5a768b3629
Piers Stuart-Kelso <piers.stuart-kelso@sky.uk>
2018-07-21 17:41:31 +0100

    adds homecave + parallax foregrounds

---
+++ Wall-E/Assets/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	public float parallaxFactor;
+
+	private GameObject player;
+	private float playerXPosition;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		playerXPosition = player.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float playerDifference = player.transform.position.x - playerXPosition;
+
+		gameObject.transform.Translate(new Vector2(- playerDifference * parallaxFactor, 0));
+
+		playerXPosition = player.transform.position.x;
+	}
+}
